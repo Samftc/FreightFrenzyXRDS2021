0336e1d003469c84c76e0bbb5d95c3db155dcfab
Samftc <sbigham2022@crossroadscollegeprep.org>
2021-11-20 13:00:32 -0600

    Added basic Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareAuto.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name = "HardwareAuto")
+public class HardwareAuto extends LinearOpMode {
+
+    HardwareOmni        bot = new HardwareOmni();
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    double pos;
+
+
+
+    @Override
+    public void runOpMode() {
+        bot.init(hardwareMap);
+
+        bot.BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bot.BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bot.FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bot.FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        bot.BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bot.BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bot.FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bot.FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        waitForStart();
+
+        while(pos < 1000){
+            bot.BR.setPower(1);
+
+            pos = bot.BR.getCurrentPosition();
+
+
+        }
+
+        bot.BR.setPower(0);
+
+
+
+
+
+    }
+}
