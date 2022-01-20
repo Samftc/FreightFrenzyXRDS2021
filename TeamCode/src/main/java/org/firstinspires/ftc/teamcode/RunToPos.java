f726c4fb6915ebb7da28530a19cc400eefe3eff6
Samftc <sbigham2022@crossroadscollegeprep.org>
2022-01-20 17:01:00 -0600

    edited runpos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToPos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToPos.java
@@ -41,8 +41,8 @@ public class RunToPos extends LinearOpMode {
         waitForStart();
 
 
-        drive(-1,-1000);
-        drive(-0.4,-500);
+        drive(-7,-1000);
+        drive(-0.4,-1000);
 
         //turn(1,2100); //turn part is useless, 2,100 should be 90°
 
